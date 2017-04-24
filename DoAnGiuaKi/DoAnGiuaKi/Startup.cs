b40826a974e337a7986cf7c809c2b6fc3b54dcc8
Mai Thụy Anh Thư <maithuyanhthu@gmail.com>
2017-04-24 21:39:33 +0700

    tao lay out, trang san pham, trang chi tiet, thiet ke csdl

---
+++ DoAnGiuaKi/DoAnGiuaKi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnGiuaKi.Startup))]
+namespace DoAnGiuaKi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
